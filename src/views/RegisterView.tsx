67f045490d2fa32bfbf03098fbc69f0ec7648e5e
Juan Vasquez <jd.vasquezh@uniandes.edu.co>
2025-01-21 23:34:56 -0500

    Manejo de errores con el hook useForm de React Hook Form

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 export default function RegisterView() {
+
+    const { register, watch, handleSubmit, formState: {errors} } = useForm()
+
+    const handleRegister = () => {
+        console.log('Desde handleRegister')
+    }
+
     return (
         <>
             <h1 className='text-4xl text-white font-bold'>Crear Cuenta</h1>
             <form
-                onSubmit={() => { }}
+                onSubmit={handleSubmit(handleRegister)}
                 className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
             >
                 <div className="grid grid-cols-1 space-y-3">
@@ -15,7 +23,11 @@ export default function RegisterView() {
                         type="text"
                         placeholder="Tu Nombre"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('name', {
+                            required: "El nombre es obligatorio"
+                        })}
                     />
+                    {errors.name && String(errors.name.message)}
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
@@ -24,7 +36,11 @@ export default function RegisterView() {
                         type="email"
                         placeholder="Email de Registro"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('email', {
+                            required: "El Email es obligatorio"
+                        })}
                     />
+                    {errors.email && String(errors.email.message)}
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
@@ -33,7 +49,11 @@ export default function RegisterView() {
                         type="text"
                         placeholder="Nombre de usuario: sin espacios"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('handle', {
+                            required: "El handle es obligatorio"
+                        })}
                     />
+                    {errors.handle && String(errors.handle.message)}
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
@@ -42,17 +62,25 @@ export default function RegisterView() {
                         type="password"
                         placeholder="Password de Registro"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password', {
+                            required: "La contraseña es obligatoria"
+                        })}
                     />
+                    {errors.password && String(errors.password.message)}
                 </div>
 
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
                     <input
-                        id="password"
+                        id="password_confirmation"
                         type="password"
                         placeholder="Repetir Password"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password', {
+                            required: "La contraseña es obligatoria"
+                        })}
                     />
+                    {errors.password && String(errors.password.message)}
                 </div>
 
                 <input
@@ -61,6 +89,7 @@ export default function RegisterView() {
                     value='Crear Cuenta'
                 />
             </form>
+
             <nav className='mt-10'>
                 <Link
                     className='text-center text-white text-lg block'
